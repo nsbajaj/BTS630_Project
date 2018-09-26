7aed4f2a5055ddac39e959a8374b5c68d16acfd9
cagomezr <carlos.a.gomez.romero@gmail.com>
2018-09-26 12:36:21 -0400

    new blade  file
    user blade file organized  properly

--- CNC/resources/views/user/user.blade.php
+++ CNC/resources/views/user/user.blade.php
@@ -1,76 +1,77 @@
 @extends('layout')
 <!DOCTYPE html>
-<html lang="en">
+<!---<html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	<title>User</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<style>
-	body{
-		padding-top: 120px;}
-		.invalid-feedback{font-size:1.15em;padding: 0.75em;}
-		footer{margin-top: 70px;
+		body {
+			padding-top: 120px;
+		}
+
+		.invalid-feedback {
+			font-size: 1.15em;
+			padding: 0.75em;
+		}
+
+		footer {
+			margin-top: 70px;
 			border-top: #919aa1 solid 1px;
 			padding-top: 2em;
 		}
+
 	</style>
 </head>
-<body>
-	@section('content')	
-	<div class="container">
-		@if(Auth::check() && Auth::user()->role_id == 1)
-		<div class="bs-docs-section">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="page-header">
-						<h1 id="tables">Users</h1>
+
+<body>-->
+@section('content')
+<div class="container">
+	@if(Auth::check() && Auth::user()->role_id == 1)
+	<div class="bs-docs-section">
+		<div class="row justify-content-center">
+			<div class="col-md-6 col-md-offset-3 ">
+				<div class="panel panel-default ">
+					<div class="panel-heading text-center">
+						<h1>
+							user</h1>
 					</div>
-					<div class="bs-component">
-						<table class="table table-hover">
-							<thead>
-								<tr>
-									<th scope="col">Name</th>
-									<th scope="col">Username</th>
-									<th scope="col">Email</th>
-									<th scope="col">Role</th>
-								</tr>
-							</thead>
-							<tbody>
-								@if(!empty($user))
-									<tr class="table-active">
-										<th scope="row">{{ $user->first_name . ' ' . $user->last_name }}</th>
-										<td>{{ $user->username }}</td>
-										<td>{{ $user->email }}</td>
-										@if($user->role_id === 1){
-											<td>Administrator</td>
-										}
-										@elseif($user->role_id == 2){
-											<td>Buyer</td>
-										}
-									    @elseif($user->role_id == 3){
-											<td>Seller</td>
-										}
-									    @endif
-								</tr>
-									
-								@endif
-								<!--
-								<tr>
-									<th scope="row">Default</th>
-									<td>Column content</td>
-									<td>Column content</td>
-									<td>Column content</td>
-								</tr>
-							-->
-							</tbody>
-						</table> 
+
+					<div class="panel-body">
+
+						@if(!empty($user))
+						<!--<form class="form-horizontal" method="POST" action="">-->
+						<div class="form-group">
+							<label for="name" class="col-md-6 control-label">User Name</label>{{ $user->first_name . ' ' . $user->last_name }}</div>
+						<div class="form-group">
+							<label for="username" class="col-md-6 control-label">Username</label>{{ $user->username }}</div>
+						<div class="form-group">
+							<label for="email" class="col-md-6 control-label">Email</label>{{ $user->email }}
+						</div>
+						<div class="form-group">
+							<label for="role" class="col-md-6 control-label">Role</label>
+							@if($user->role_id === 1){
+							Administrator
+							}
+							@elseif($user->role_id == 2){
+							Buyer
+							}
+							@elseif($user->role_id == 3){
+							Seller
+							}
+							@endif
+						</div>
+						<!-- form-->
+						@endif
+
 					</div>
 				</div>
 			</div>
 		</div>
-		@endif
-		@endsection
+	</div>
+	@endif
+</div>
 
-	</body>
-	</html>
+@endsection
