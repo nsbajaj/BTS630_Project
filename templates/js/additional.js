66ef80fe9637dcce978f2e1a9f861b10530e67e0
cagomezr <carlos.a.gomez.romero@gmail.com>
2018-11-11 15:26:46 -0500

    A POPPING MENU INSTANCE
    having issues with pc but the framework is there based on snippet request

--- templates/js/additional.js
+++ templates/js/additional.js
@@ -7,6 +7,16 @@ $(document).ready(function () {
 					return "";
            });
 	*/
+	  $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
 	var errormesg="";
 	$(".login form-group form").on('submit', function(){
 		var retval;
